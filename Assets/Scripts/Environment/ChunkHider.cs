b23b09fe34c8958b8419ce8386529b8492aee340
Paul N <deul1st.official@gmail.com>
2025-08-31 16:16:51 +1000

    basic world exploring
    basic world exploring has been implemented, not final map or props

---
+++ Assets/Scripts/Environment/ChunkHider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkHider : MonoBehaviour
+{
+    private Terrain[] chunks;       // array of all the chunks
+
+    public float visibleDistance;   // max distance a chunk will be visible
+    public int chunkSize;           // width/length of each chunk
+    public float checkRate;         // how often do we check the chunks?
+
+    void Start ()
+    {
+        // get all the chunks
+        chunks = FindObjectsOfType<Terrain>();
+
+        // check the chunks every 'checkRate' seconds
+        InvokeRepeating("CheckChunks", 0.0f, checkRate);
+    }
+
+    // checks which chunks should be enabled / disabled based on distance from the player
+    void CheckChunks ()
+    {
+        // get the player position with a Y axis of 0
+        Vector3 playerPos = transform.position;
+        playerPos.y = 0;
+
+        // loop through each chunk
+        foreach(Terrain chunk in chunks)
+        {
+            // get the center position of the chunk
+            Vector3 chunkCenterPos = chunk.transform.position + new Vector3(chunkSize / 2, 0, chunkSize / 2);
+
+            // check the distance
+            if(Vector3.Distance(playerPos, chunkCenterPos) > visibleDistance)
+                chunk.gameObject.SetActive(false);
+            else
+                chunk.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/ObjectHider.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHider : MonoBehaviour
+{
+    private Bounds bounds;                  // bounds of the terrain
+    private GameObject[] hidableObjects;    // dynamic objects we can hide
+
+    void Awake ()
+    {
+        // get the hidable objects
+        hidableObjects = GameObject.FindGameObjectsWithTag("ChunkObject");
+
+        // get the bounds of our terrain
+        Terrain terrain = GetComponent<Terrain>();
+
+        Vector3 dimensions = new Vector3(terrain.terrainData.heightmapResolution, 1000, terrain.terrainData.heightmapResolution);
+        Vector3 pos = transform.position + new Vector3(dimensions.x / 2, 0, dimensions.z / 2);
+
+        bounds = new Bounds(pos, dimensions);
+    }
+
+    // called when the object gets enabled
+    void OnEnable ()
+    {
+        // loop through each hidable object
+        foreach(GameObject obj in hidableObjects)
+        {
+            // is the object inside the chunk bounds?
+            if(obj != null && bounds.Contains(obj.transform.position))
+                obj.SetActive(true);
+        }
+    }
+
+    // called when the object gets disabled
+    void OnDisable ()
+    {
+        // loop through each hidable object
+        foreach(GameObject obj in hidableObjects)
+        {
+            // is the object inside the chunk bounds?
+            if(obj != null && bounds.Contains(obj.transform.position))
+                obj.SetActive(false);
+        }
+    }
+}
